0834ba98ae17b4c6013451f003be251315e4df9c
Shadab Arzoo <shadabarzoo6@gmail.com>
2024-12-15 21:33:08 +0530

    first commit

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the GymFluencer and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'MATHEW HENDRICKSON',
+    status : 'ENTREPRENEUR'
+  },
+  {
+    image: image2,
+    review: 'I took the first step with GymFluencer, and with the perfect plan and dedication, I transformed my body like never before!',
+    name: 'JOHN KEVIN',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:'By trusting GymFluencer with the right program and plan, I achieved my dream physique and feel stronger than ever!',
+    name: 'FRANKLIN',
+    status: "CUSTOMER"
+  }
+];
